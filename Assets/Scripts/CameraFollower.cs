1636b9456ef189e34f2c5b652ec7cff8db8d861d
trung nguyen ba <tantrungnb@gmail.com>
2014-11-20 09:28:08 -0600

    commit

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollower : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform player;
+	public float distance;
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (player.position.x + distance,
+		                                     this.transform.position.y,
+		                                     this.transform.position.z);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float velocity;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 speed = Vector3.right * velocity;
+		speed *= Time.deltaTime;
+		this.transform.Translate (speed);
+	}
+}
