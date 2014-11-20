b25a68eb6cdb3044cace7c6d5e0b6125fdaede8c
trung nguyen ba <tantrungnb@gmail.com>
2014-11-20 12:38:56 -0600

    commit

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+	public Transform player;
+	public float dis;
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (player.position.x-dis,
+		                                     this.transform.position.y,
+		                                     this.transform.position.z);
+	}
+	void OnTriggerEnter2D(Collider2D col){
+		if ((col.gameObject.tag != "ground")&&(col.gameObject.tag != "Player")) {
+					Destroy (col.gameObject);
+				}
+		}
+
+}
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Point : MonoBehaviour {
+
+	// Use this for initialization
+
+	public static float point;
+	public float rateOfChange;
+	public static float level;
+	private float changeLevel;
+	public float rateOCL;
+	// Update is called once per frame
+	void Start(){
+		point = 0;
+		changeLevel=100;
+		//rateOCL = 20;
+		level = 0;
+	}
+	void Update () {
+		point += rateOfChange * Time.deltaTime*10;
+		if (point >= changeLevel) {
+						level++;
+						changeLevel += 100*rateOCL;
+						rateOCL++;
+				}
+	}
+	void OnGUI(){
+		int t = (int)point;
+				Rect pointbox = new Rect (Screen.width/2-40f, 0f, 80f, 20f);
+				GUI.Box(pointbox, "" + t.ToString ());
+				Rect levelbox = new Rect (0f, 0f, 80f, 20f);
+				GUI.Box(levelbox, "" + level.ToString ());
+		}
+}
