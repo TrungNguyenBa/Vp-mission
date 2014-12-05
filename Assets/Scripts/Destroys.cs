15d7886abbf79e7f86e5ef4f5ce085e758905044
trung nguyen ba <tantrungnb@gmail.com>
2014-12-04 23:06:41 -0600

    check and see

---
+++ Assets/Scripts/Destroys.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroys : MonoBehaviour {
+	public Transform player;
+	public float dis;
+	// Update is called once per frame
+
+	void Update () {
+		this.transform.position = new Vector3 (player.position.x-dis,
+		                                     this.transform.position.y,
+		                                     this.transform.position.z);
+	
+	}
+	void OnTriggerEnter2D(Collider2D col){
+
+		if ((col.gameObject.tag != "Destroy")&&(col.gameObject.tag != "Player")) {
+		
+			Destroy(col.gameObject);
+				}
+		}
+
+}
