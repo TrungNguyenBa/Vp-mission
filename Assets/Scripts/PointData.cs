89569efc7418801fbafe7109a26e4c599595d4f9
trung nguyen ba <tantrungnb@gmail.com>
2015-07-27 13:59:04 -0500

    Please enter the commit message for your changes. Lines starting

---
+++ Assets/Scripts/PointData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class PointData : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
+
